77b597d63956dc63667b36051ef9f0132bea9a46
pius lucky <ptagbajumi12@gmail.com>
2023-12-21 16:19:50 +0100

    new

--- travel-app/src/Components/Footer/Footer.jsx
+++ travel-app/src/Components/Footer/Footer.jsx
@@ -1,192 +1,173 @@
-import React,{useEffect} from 'react'
-import './footer.css'
-import video1 from '../../Assets/plan.mp4'
-import { FiSend } from "react-icons/fi";
+import React, { useEffect } from 'react';
+import './footer.css';
+import planVideo from '../../Assets/plan.mp4';
+import { FiSend } from 'react-icons/fi';
 import { MdOutlineTravelExplore } from 'react-icons/md';
-import { FiFacebook } from "react-icons/fi";
-import { FaInstagram } from "react-icons/fa";
-import { FaWhatsapp } from "react-icons/fa";
-import { FaXTwitter } from "react-icons/fa6";
-import { FaLinkedin } from "react-icons/fa6";
-import { FaChevronRight } from "react-icons/fa6";
+import { FiFacebook } from 'react-icons/fi';
+import { FaInstagram } from 'react-icons/fa';
+import { FaWhatsapp } from 'react-icons/fa';
+import { FaXTwitter } from 'react-icons/fa6';
+import { FaLinkedin } from 'react-icons/fa6';
+import { FaChevronRight } from 'react-icons/fa6';
 
-import Aos from 'aos'
-import 'aos/dist/aos.css'
+import Aos from 'aos';
+import 'aos/dist/aos.css';
 
-
-
- const Footer = () => {
-
-  useEffect(()=>{
-    Aos.init({duration:2000})
-  }, [])
-
+const Footer = () => {
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+  }, []);
 
   return (
-   <section className="footer">
-    <div className="videoDiv">
+    <section className="footer">
+      <div className="videoDiv">
+        <video src={planVideo} muted autoPlay loop type="video/mp4" className="video"></video>
+      </div>
 
-    {/* <img src={img} alt="" className='imgC' /> */}
+      <div className="setcontent container">
+        <div className="contactDiv flex">
+          <small data-aos="fade-up" className="small">
+            KEEP IN TOUCH
+          </small>
+          <h2 data-aos="fade-up" className="head2">
+            Travel with us
+          </h2>
 
-    <video src={video1} muted autoPlay loop typeof='video/mp4' className='video'></video>
+          <div className="inputDiv flex">
+            <input data-aos="fade-up" type="text" placeholder="Enter Email Address" className="input" />
+            <button data-aos="fade-up" className="btn flex" type="submit">
+              SEND<FiSend className="icon" />
+            </button>
+          </div>
+        </div>
 
+        <div className="footerCard flex">
+          <div className="footerIntro flex">
+            <div className="logoDiv">
+              <a href="/" className="logo flex">
+                <MdOutlineTravelExplore className="icon" />
+                Travels
+              </a>
+            </div>
 
-  
+            <div data-aos="fade-up" className="footerParagraph">
+              gvdwhjjckhbkj bzjb bzkc bakbasjkbcknKKB kjbjbkjbkbkjb
+            </div>
 
-     </div>
+            <div data-aos="fade-up" className="footerSocials">
+              <FiFacebook className="icon" />
+              <FaInstagram className="icon" />
+              <FaWhatsapp className="icon" />
+              <FaXTwitter className="icon" />
+              <FaLinkedin className="icon" />
+            </div>
+          </div>
 
-    
-     <div className="setcontent container">
-      <div className="contactDiv flex">
-        <small data-aos ="fade-up" className='small'>KEEP IN TOUCH</small>
-        <h2 data-aos ="fade-up" className='head2'>Travel with us</h2>
+          <div data-aos="fade-up" data-aos-duration="3000" className="footerLinks grid">
+            <div className="linkGroup">
+              <span className="groupTitle">OUR AGENCY</span>
 
-        <div className="inputDiv flex">
-          <input data-aos ="fade-up" type="text" placeholder='Enter Email Address' className='input'/>
-         <button data-aos ="fade-up" className="btn flex" type='submit'>
+              <ul>
+                <li className="footerlist flex">
+                  <FaChevronRight className="icon" />
+                  Services
+                </li>
 
-         SEND<FiSend className='icon'/>
+                <li className="footerlist flex">
+                  <FaChevronRight className="icon" />
+                  Insurance
+                </li>
 
-         </button>
+                <li className="footerlist flex">
+                  <FaChevronRight className="icon" />
+                  Agencies
+                </li>
+
+                <li className="footerlist flex">
+                  <FaChevronRight className="icon" />
+                  Tourism
+                </li>
+
+                <li className="footerlist flex">
+                  <FaChevronRight className="icon" />
+                  Payment
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <div data-aos="fade-up" data-aos-duration="4000" className="footerLinks grid">
+            <div className="linkGroup">
+              <span className="groupTitle">PARTNERS</span>
+
+              <ul>
+                <li className="footerlist flex">
+                  <FaChevronRight className="icon" />
+                  Bookings
+                </li>
+
+                <li className="footerlist flex">
+                  <FaChevronRight className="icon" />
+                  trivago
+                </li>
+
+                <li className="footerlist flex">
+                  <FaChevronRight className="icon" />
+                  Wakanow
+                </li>
+
+                <li className="footerlist flex">
+                  <FaChevronRight className="icon" />
+                  Flynow
+                </li>
+
+                <li className="footerlist flex">
+                  <FaChevronRight className="icon" />
+                  Netlify
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <div data-aos="fade-up" data-aos-duration="5000" className="footerLinks grid">
+            <div className="linkGroup">
+              <span className="groupTitle">LAST MINUTE</span>
+
+              <button className="footerButton flex">
+                <FaChevronRight className="icon" />
+                London
+              </button>
+
+              <button className="footerButton flex">
+                <FaChevronRight className="icon" />
+                Texas
+              </button>
+
+              <button className="footerButton flex">
+                <FaChevronRight className="icon" />
+                Canada
+              </button>
+
+              <button className="footerButton flex">
+                <FaChevronRight className="icon" />
+                Berlin
+              </button>
+
+              <button className="footerButton flex">
+                <FaChevronRight className="icon" />
+                Paris
+              </button>
+            </div>
+          </div>
+
+          <div className="footerDiv flex">
+            <small className="smDiv">BEST TRAVEL WEBSITE THEME</small>
+            <small className="smDiv">COPY RIGHT RESERVED - ISRATECH 2023</small>
+          </div>
         </div>
       </div>
-      
-      <div className="footerCard flex">
-        <div className="footerIntro flex">
-          <div className="logoDiv">
-            <a href="" className="logo flex">
-            <MdOutlineTravelExplore className="icon"/>Travels
-            </a>
-          </div>
-
-          <div data-aos ="fade-up" className="footerParagraph">
-            gvdwhjjckhbkj bzjb bzkc bakbasjkbcknKKB kjbjbkjbkbkjb
-          </div>
-
-          <div data-aos ="fade-up" className="footerSocials">
-          <FiFacebook className='icon'/>
-          <FaInstagram className='icon' />
-          <FaWhatsapp className='icon'/>
-          <FaXTwitter  className='icon'/>
-
-          <FaLinkedin className='icon'/>
-          
-
-          </div>
-        </div>
-
-         {/* // group 1 */}
-        <div data-aos="fade-up" data-aos-duration="3000"className="footerLinks grid">
-          <div className="linkGroup">
-            <span className="groupTitle">
-              OUR AGENCY
-            </span>
-
-            <li className="footerlist flex">
-            <FaChevronRight className='icon' />
-            Services
-            </li>
-
-            <li className="footerlist flex">
-            <FaChevronRight className='icon' />
-            Insurance
-            </li>
-
-            <li className="footerlist flex">
-            <FaChevronRight className='icon' />
-            Agencies
-            </li>
-
-            <li className="footerlist flex">
-            <FaChevronRight className='icon' />
-            Tourisim
-            </li>
-
-            <li className="footerlist flex">
-            <FaChevronRight className='icons' />
-            Payment
-            </li>
-          </div>
-        </div>
-
-         {/* // group 2 */}
-         <div data-aos="fade-up" data-aos-duration="4000" className="footerLinks grid">
-          <div className="linkGroup">
-            <span className="groupTitle">
-              PARTNERS
-            </span>
-
-            <li className="footerlist flex">
-            <FaChevronRight className='icon' />
-            Bookings
-            </li>
-
-            <li className="footerlist flex">
-            <FaChevronRight className='icon' />
-            trivago
-            </li>
-
-            <li className="footerlist flex">
-            <FaChevronRight className='icon' />
-            Wakanow
-            </li>
-
-            <li className="footerlist flex">
-            <FaChevronRight className='icon' />
-            Flynow
-            </li>
-
-            <li className="footerlist flex">
-            <FaChevronRight className='icons' />
-            Netlify
-            </li>
-          </div>
-        </div>
-
-         {/* // group 3 */}
-         <div data-aos="fade-up" data-aos-duration="5000" className="footerLinks grid">
-          <div className="linkGroup">
-            <span className="groupTitle">
-              LAST MINUTE
-            </span>
-
-            <li className="footerlist flex">
-            <FaChevronRight className='icon' />
-            London
-            </li>
-
-            <li className="footerlist flex">
-            <FaChevronRight className='icon' />
-            Texas
-            </li>
-
-            <li className="footerlist flex">
-            <FaChevronRight className='icon' />
-            Canada
-            </li>
-
-            <li className="footerlist flex">
-            <FaChevronRight className='icon' />
-            Berlin
-            </li>
-
-            <li className="footerlist flex">
-            <FaChevronRight className='icons' />
-            Paris
-            </li>
-          </div>
-        </div>
-
-        <div className="footerDiv flex">
-          <small className='smDiv'>BEST TRAVEL WEBSITE THEME</small>
-          <small className='smDiv'>COPY RIGHT RESERVED - ISRATECH 2023</small>
-        </div>
-      </div>
-     </div>
-   
-   </section>
-
-  )
-}
+    </section>
+  );
+};
 
 export default Footer;
--- travel-app/src/Components/Navbar/Navbar.jsx
+++ travel-app/src/Components/Navbar/Navbar.jsx
@@ -1,87 +1,61 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './navbar.css';
 
 import { MdOutlineTravelExplore } from 'react-icons/md';
 import { FaWindowClose } from "react-icons/fa";
 import { TbGridDots } from "react-icons/tb";
 
-
-
 const Navbar = () => {
+    const [active, setActive] = useState('navBar');
 
-    const [active, setActive] = useState ('navBar')
-
-    const showNAV = ()=>{
-        setActive ('navBar activeNavbar')
+    const showNAV = () => {
+        setActive('navBar activeNavbar');
     }
 
-    const removNAV = ()=>{
-        setActive ('navBar')
+    const removNAV = () => {
+        setActive('navBar');
     }
 
-  return (
-  <section className="navBarSection">
-    <header className="header flex">
-        <div className="logoDiv">
-            <a href="" className="logo flex">
-                <h1><MdOutlineTravelExplore className="icon"/>Travels</h1>
-
-            </a>
-        </div>
-
-        <div className={active}>
-            <ul className="navLists flex">
-               <li className="navItem">
-                <a href="" className="navLink">Home</a>
-               </li>
-
-               <li className="navItem">
-                <a href="" className="navLink">Packages</a>
-               </li>
-
-               <li className="navItem">
-                <a href="" className="navLink">Shop</a>
-               </li>
-
-               <li className="navItem">
-                <a href="" className="navLink">About</a>
-               </li>
-
-               <li className="navItem">
-                <a href="" className="navLink">Pages</a>
-               </li>
-
-               <li className="navItem">
-                <a href="" className="navLink">News</a>
-               </li>
-               
-               <li className="navItem">
-                <a href="" className="navLink">Contact</a>
-               </li>
-               
-               <button className='btn'>
-                         <a href="">BOOK NOW</a>
-            
+    return (
+        <section className={`navBarSection ${active}`}>
+            <header className="header flex">
+                <div className="logoDiv">
+                    <button className="logo flex" onClick={() => window.location.href="/"}>
+                        <h1><MdOutlineTravelExplore className="icon" />Travels</h1>
                     </button>
+                </div>
 
-            </ul>
-            <div onClick={removNAV} className='closeNavbar'>
-                <FaWindowClose className='icon' />
-             </div>
-        </div>
-        
-        <div onClick={showNAV} className="toggleNavbar">
-         
-        <div className="toggleNavbar">
-            <TbGridDots className='icon' />
-            </div>
+                <ul className="navLists flex">
+                    <NavItem href="/">Home</NavItem>
+                    <NavItem href="/packages">Packages</NavItem>
+                    <NavItem href="/shop">Shop</NavItem>
+                    <NavItem href="/about">About</NavItem>
+                    <NavItem href="/pages">Pages</NavItem>
+                    <NavItem href="/news">News</NavItem>
+                    <NavItem href="/contact">Contact</NavItem>
+                    <button className='btn'>
+                        <a href="/book-now">BOOK NOW</a>
+                    </button>
+                </ul>
 
-        </div>
+                <div onClick={removNAV} className='closeNavbar'>
+                    <FaWindowClose className='icon' />
+                </div>
 
-    </header>
-  </section>
-
-  )
+                <div onClick={showNAV} className="toggleNavbar">
+                    <div className="toggleNavbar">
+                        <TbGridDots className='icon' />
+                    </div>
+                </div>
+            </header>
+        </section>
+    );
 }
 
-export default Navbar
+const NavItem = ({ href, children }) => (
+    <li className="navItem">
+        <a href={href} className="navLink">{children}</a>
+    </li>
+);
+
+export default Navbar;
